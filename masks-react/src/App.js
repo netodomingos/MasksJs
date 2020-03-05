ba411c579e7feb1e720954ae6cc956e5ee8c0d81
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2020-03-05 16:29:35 -0300

    Update App.js

--- masks-react/src/App.js
+++ masks-react/src/App.js
@@ -9,31 +9,19 @@ import './App.css';
 export default function App(){
 
   const [ cpf, setCpf ] = useState('')
-  const [ phoneNumber, setPhoneNumer ] = useState('')
+  const [ phoneNumber, setPhoneNumber ] = useState('')
   const [ cnpj, setCnpj ] = useState('')
-
-  const setCpfValue = event =>{ 
-    setCpf(cpfMask(event))
-  }
-
-  const setPhoneValue = event => {
-   setPhoneNumer(phoneMask(event))
-  }
-
-  const setCnpjValue = event => {
-    setCnpj(cnpjMask(event))
-  }
-
+  
     return (
       <div className='input-div'>
         <div className='intern-div'>
           <h1>React Masks</h1>
           <h3>CPF:</h3>
-          <input type='text' maxLength='14' name='cpf' value={cpf} onChange={e => setCpfValue(e.target.value)}/>
+          <input type='text' maxLength='14' name='cpf' value={cpf} onChange={e => setCpf(cpfMask(e.target.value))}/>
           <h3>Telefone:</h3>
-          <input type='text' maxLength='15' name='telefone' value={phoneNumber} onChange={e => setPhoneValue(e.target.value)}/>
+          <input type='text' maxLength='15' name='telefone' value={phoneNumber} onChange={e => setPhoneNumber(phoneMask(e.target.value))}/>
           <h3>CNPJ</h3>
-          <input type='text' maxLength='18' name='telefone' value={cnpj} onChange={e => setCnpjValue(e.target.value)}/>
+          <input type='text' maxLength='18' name='telefone' value={cnpj} onChange={e => setCnpj(cnpjMask(e.target.value))}/>
         </div>
       </div>
     );
